21304efd249d33084bc21f7e9f9931319ea0f13c
qyy <qyy@code.com>
2021-10-20 15:35:57 +0800

    hot fix commit-2

--- src/main/java/com/code/App.java
+++ src/main/java/com/code/App.java
@@ -9,6 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello git hot fix!" );
+        System.out.println( "Hello git hot fix!" );
+        System.out.println( "Hello git hot fix!" );
+        System.out.println( "Hello git hot fix! text" );
 
 
     }
