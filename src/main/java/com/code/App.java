a3c112351445e4d682ae6eaa12bb66314df16f33
qyy <qyy@code.com>
2021-10-20 15:37:05 +0800

    master commit2

--- src/main/java/com/code/App.java
+++ src/main/java/com/code/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello git hot fix!" );
+        System.out.println( "Hello git master" );
 
 
     }
